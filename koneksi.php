3d33a80643d963c9a995e47e3e44a44eb0c4f497
ichaadellia <48471700+ichaadellia@users.noreply.github.com>
2019-04-03 08:32:54 +0700

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php 
+$koneksi = mysqli_connect("localhost","id8951506_wisata1","1234567","id8951506_wisata");
+ 
+// Check connection
+if (mysqli_connect_errno()){
+	echo "Koneksi database gagal : " . mysqli_connect_error();
+}
+ 
+?>
